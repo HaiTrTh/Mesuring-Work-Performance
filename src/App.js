f056a75c19bfe1ae04b71cb5e49a95cb6c7d7a91
haitrth <haithanhtran00@gmail.com>
2024-11-23 14:57:09 +0700

    Mesuring-Work-Performance app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Table, Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [task, setTasks] = useState([]);
+const [taskName, setTaskName] = useState('');
+const [importance, setImportance] = useState(1);
+const [emergency, setEmergency] = useState(1);
+const [difficulty, setDifficulity] = useState({
+  done: false,
+  knowledge: false,
+  acquaintance : false, 
+})
+const handleSubmit = () => {
+  setTasks([...tasks])
 }
-
-export default App;
